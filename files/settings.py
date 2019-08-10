340f155381748635c3ac7c282be728e875556a10
nareshgoli <venkatanaresh515@gmai.com>
2019-08-11 00:09:24 +0530

    login errors corrected

--- files/settings.py
+++ files/settings.py
@@ -77,24 +77,19 @@ WSGI_APPLICATION = 'files.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'codefork',
+        'USER': 'codeforkuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'codefork',
-#         'USER': 'codeforkuser',
-#         'PASSWORD': 'password',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
-# }
 
 
 # Password validation
